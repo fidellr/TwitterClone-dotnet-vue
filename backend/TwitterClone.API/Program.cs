14199219448c6483e9ee9f1c4652bc5e47fa0dd4
fidellr <fidelramadhan@gmail.com>
2026-02-22 16:26:28 +0700

    chore: add Dockerfile for backend and frontend deployment and fix cors policy

--- backend/TwitterClone.API/Program.cs
+++ backend/TwitterClone.API/Program.cs
@@ -41,7 +41,15 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
 });
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddCors(options => options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -52,7 +60,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors();
+app.UseCors("AllowAll");
 
 app.UseWebSockets(new WebSocketOptions { KeepAliveInterval = TimeSpan.FromMinutes(2) });
 
@@ -91,4 +99,11 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<TwitterDbContext>();
+    context.Database.Migrate();
+}
 app.Run();
